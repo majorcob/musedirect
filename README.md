8e0e509d626617ade5b6c68d89059bf9228130d8
Jacob Cazabon <j@cob.im>
2018-09-24 18:00:31 -0400

    Mom said I still need semicolons in ES2016

--- README.md
+++ README.md
@@ -12,18 +12,18 @@ Provides an event-based interface to OSC data sent by [Muse Direct](https://www.
 
 ```js
 // Import class
-const Muse = require("musedirect")
+const Muse = require("musedirect");
 
 // Create new instance listening at 127.0.0.1:7000
-let muse = new Muse("127.0.0.1", 7000)
+let muse = new Muse("127.0.0.1", 7000);
 
 // Create event listeners; see list of events below
 muse.on("open", () => {
 	// ...
-})
+});
 
 // Start listening
-muse.start()
+muse.start();
 ```
 
 ## API
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // JACOB CAZABON 2018
 
-const osc = require("osc")
-const EventEmitter = require("events").EventEmitter
+const osc = require("osc");
+const EventEmitter = require("events").EventEmitter;
 
 class Muse extends EventEmitter {
 
 	constructor(address="127.0.0.1", port=7000) {
 
 		// Built-in EventEmitter stuff
-		super()
+		super();
 
 		// UDP connection info
-		this.address = address
-		this.port = port
+		this.address = address;
+		this.port = port;
 		this.udp = new osc.UDPPort({
 			localAddress: this.address,
 			localPort: this.port
-		})
+		});
 
 	}
 
@@ -24,113 +24,113 @@ class Muse extends EventEmitter {
 
 		// Open connection
 		this.udp.on("ready", () => {
-			this.emit("open")
-		})
+			this.emit("open");
+		});
 
 		// Emit generic message and OSC address events
 		this.udp.on("message", (msg) => {
-			this.emit("message", msg)
-			this.emit(msg.address, msg.args)
-		})
+			this.emit("message", msg);
+			this.emit(msg.address, msg.args);
+		});
 
 		// Raw EEG data is received
 		this.on("/eeg", (args) => {
-			let [tp9, af7, af8, tp10] = args
-			this.emit("eeg raw", tp9, af7, af8, tp10)
-		})
+			let [tp9, af7, af8, tp10] = args;
+			this.emit("eeg raw", tp9, af7, af8, tp10);
+		});
 
 		// Filtered EEG data is received
 		this.on("/notch_filtered_eeg", (args) => {
-			let [tp9, af7, af8, tp10] = args
-			this.emit("eeg filtered", tp9, af7, af8, tp10)
-		})
+			let [tp9, af7, af8, tp10] = args;
+			this.emit("eeg filtered", tp9, af7, af8, tp10);
+		});
 
 		// EEG quantizer is received
 		this.on("/eeg/quantization", (args) => {
-			let [stepsize] = args
-			this.emit("eeg stepsize", stepsize)
-		})
+			let [stepsize] = args;
+			this.emit("eeg stepsize", stepsize);
+		});
 
 		// Accelerometer data is received
 		this.on("/acc", (args) => {
-			let [accelX, accelY, accelZ] = args
-			this.emit("accel data", accelX, accelY, accelZ)
-		})
+			let [accelX, accelY, accelZ] = args;
+			this.emit("accel data", accelX, accelY, accelZ);
+		});
 
 		// Gyroscope data is received
 		this.on("/gyro", (args) => {
-			let [rollRate, pitchRate, yawRate] = args
-			this.emit("gyro data", rollRate, pitchRate, yawRate)
-		})
+			let [rollRate, pitchRate, yawRate] = args;
+			this.emit("gyro data", rollRate, pitchRate, yawRate);
+		});
 
 		// For each band:
 		["delta", "theta", "alpha", "beta", "gamma"].forEach((band) => {
 
 			// Absolute band power is received
 			this.on(`/elements/${band}_absolute`, (args) => {
-				let [tp9, af7, af8, tp10] = args
-				this.emit(`${band} abp`, tp9, af7, af8, tp10)
-			})
+				let [tp9, af7, af8, tp10] = args;
+				this.emit(`${band} abp`, tp9, af7, af8, tp10);
+			});
 
 			// Relative band power is received
 			this.on(`/elements/${band}_relative`, (args) => {
-				let [tp9, af7, af8, tp10] = args
-				this.emit(`${band} rbp`, tp9, af7, af8, tp10)
-			})
+				let [tp9, af7, af8, tp10] = args;
+				this.emit(`${band} rbp`, tp9, af7, af8, tp10);
+			});
 
 			// Session score is received
 			this.on(`/elements/${band}_session_score`, (args) => {
-				let [tp9, af7, af8, tp10] = args
-				this.emit(`${band} score`, tp9, af7, af8, tp10)
-			})
-		})
+				let [tp9, af7, af8, tp10] = args;
+				this.emit(`${band} score`, tp9, af7, af8, tp10);
+			});
+		});
 
 		// Forehead contact status is received
 		this.on("/elements/touching_forehead", (args) => {
-			let [isTouching] = args
-			this.emit("contact status", !!isTouching)
-		})
+			let [isTouching] = args;
+			this.emit("contact status", !!isTouching);
+		});
 
 		// Sensor statuses are received
 		this.on("/elements/is_good", (args) => {
-			let [leftEar, leftFront, rightFront, rightEar] = args
-			this.emit("channel status", !!leftEar, !!leftFront, !!rightFront, !!rightEar)
-		})
+			let [leftEar, leftFront, rightFront, rightEar] = args;
+			this.emit("channel status", !!leftEar, !!leftFront, !!rightFront, !!rightEar);
+		});
 
 		// Battery status is received
 		this.on("/batt", (args) => {
-			let [decimal, voltage, temperature] = args
-			this.emit("battery status", decimal / 10000, voltage, temperature)
-		})
+			let [decimal, voltage, temperature] = args;
+			this.emit("battery status", decimal / 10000, voltage, temperature);
+		});
 
 		// DRL reference voltages are received
 		this.on("/drlref", (args) => {
-			let [drl, ref] = args
-			this.emit("drl status", drl, ref)
-		})
+			let [drl, ref] = args;
+			this.emit("drl status", drl, ref);
+		});
 
 		// Blink presence is received
 		this.on("/elements/blink", (args) => {
-			if (args) this.emit("blink")
-		})
+			if (args) this.emit("blink");
+		});
 
 		// Jaw clench presence is received
 		this.on("/elements/jaw_clench", (args) => {
-			if (args) this.emit("jaw clench")
-		})
+			if (args) this.emit("jaw clench");
+		});
 
-		this.udp.open()
+		this.udp.open();
 
 	}
 
 	stop() {
 
 		// Close connection
-		this.udp.close()
-		this.emit("close")
+		this.udp.close();
+		this.emit("close");
 
 	}
 
 }
 
-module.exports = Muse
+module.exports = Muse;
